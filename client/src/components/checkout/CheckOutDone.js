93b123ff30fdcd5aedc985241fb7f8c7f4be40dc
Isak Kallenbach <kallenbach13@gmail.com>
2022-08-24 16:36:55 +0100

    slight change

--- client/src/components/checkout/CheckOutDone.js
+++ client/src/components/checkout/CheckOutDone.js
@@ -12,13 +12,13 @@ const CheckOutDone = () => {
     return (
         <div>
             <div>
-                <p className="font-bold text-center text-xl my-8 text-gray-700 text-base">
+                <p className="font-bold font-serif text-center text-xl my-8 text-gray-700 text-base">
                   Payment Succesful. Order Nr: {id}
                 </p>
             </div>
             <div className="text-center">
               <button
-                  className="m-4 py-2 px-4 cursor-pointer border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  className="m-4 py-2 px-4 cursor-pointer border border-transparent shadow-sm text-sm font-medium font-serif rounded-md text-white bg-pink-300 hover:bg-pink-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-400"
                   onClick={handleKeepShoppingClick}>
                   Keep Shopping
               </button>  
